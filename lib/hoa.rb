3c5f1b0dba8cdbdf65e7900d070c62f3a8f32c37
Jessica Michael <pantherjmm@yahoo.com>
2019-11-15 07:16:40 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -10,8 +10,8 @@ BASE_HOA = {
 def add_character(show, name)
   boomerang_tv = {
     :chipmunks => ["Alvin", "Simon", "Theodore"],
-    :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
-    :jetsons => ["George", "Jane", "Judy", "Elroy"],
+    :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)", "Snarf"],
+    :jetsons => ["George", "Jane", "Judy", "Elroy", "Snarf"],
     :scooby_doo => ["Fred", "Velma", "Daphne", "Shaggy", "Scooby"]
   }
   # Write your implementation here
